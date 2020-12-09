e08807a37a04b3830032506204b40cf3f2c1ff1f
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-12-09 22:56:22 +0900

    .

--- src/test.java
+++ src/test.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class test {
 	public static void main(String[] args) {
 		System.out.println("매개변수의 개수" + args.length);
@@ -5,6 +7,13 @@ class test {
 			System.out.println("args[" + i + "] = \"" + args[i] + "\"");
 		}
 		
-		System.out.printf("출력 테스트 %n123");
+		System.out.printf("출력 테스트 %n123%n");
+		
+		String a = "abcde";
+		System.out.println(a.toCharArray());
+		
+		int[] arr = {3,2,0,1};
+		Arrays.sort(arr);
+		System.out.print(Arrays.toString(arr));
 	}
 }
