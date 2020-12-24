1e70268099a6c26c79bc29c62ee172bb382a01df
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-12-24 12:07:58 +0900

    .

---
+++ study/자바의정석studydiary/과제1224_1.java
@@ -0,0 +1,17 @@
+package 자바의정석studydiary;
+
+public class 과제1224_1 {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=9; i++) {
+			if(i%3==0) {
+				System.out.println(3);
+			}
+			else {
+				System.out.println(i%3);
+			}
+		}
+	}
+
+}
